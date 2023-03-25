b8f18dc3d5c4b7c8195e89f767535e8908f9a44c
CJ Llorente <cjllorente827@gmail.com>
2023-03-25 14:19:45 -0400

    more documentation

--- src/Task.py
+++ src/Task.py
@@ -1,8 +1,39 @@
+"""A Task is a the smallest unit of work performed by an analysis Pipeline
+
+A Task is a the smallest unit of work performed by an analysis Pipeline. 
+Tasks must be deterministic (returns a the same result for the same arguments )
+and atomic (performs all its work without depending on another Task being run concurrently).
+
+A Task can either be independent (doesn't rely on the completion of any previous Task)
+or dependent (relies on the previous completion of one or more Tasks).
+
+When a Task is created, it gets passed a function and set of positional and keyword arguments.
+This function will be executed once the Pipeline it belongs to has determined that the Task 
+is ready to run. 
+
+-----------------------------------------------------------------------------------------
+IMPORTANT
+-----------------------------------------------------------------------------------------
+
+Any arguments passed to a Task's user_function must have a unique string representation
+given by the str() function. This is due to the default behavior of the str() function
+for classes in Python, which returns the memory address of the object. That address is almost
+always unique to a specific run of the code and therefore produces indeterministic hashes
+even though the underlying data might be exactly the same.
+-----------------------------------------------------------------------------------------
+
+Once the Task completes, the return value of its function is saved to a file in the ndustria
+Cache. The filename is determined by taking a hash of the function's source code, arguments, 
+and the hashcodes of any dependencies. This way, if the code changes, the arguments change, 
+or any of the dependencies change, the Cache will not be able to find a result for it
+and the Task will be rerun. 
+"""
+
 import inspect, hashlib, time, tracemalloc
 from Logger import log, warn
 
 class Task:
-
+    """A Task is a the smallest unit of work performed by an analysis Pipeline"""
     def __init__(self, id,
         user_function, 
         args, 
@@ -11,6 +42,19 @@ class Task:
         match="most_recent",
         depends_on=None
     ):
+        """Initializes a new Task. Should not be called directly. Instead use the @AddTask decorator.
+
+        Arguments:
+        user_function -- A user defined function that executes when this Task is run. Its return value is saved to a file in the Cache
+        args -- A list of positional arguments to pass to the function. 
+        kwargs -- A list of keyword arguments to pass to the function.
+        pipeline -- A reference to the pipeline this Task belongs to. Not strictly necessary since the Pipeline is a static singleton but whatev
+        match -- a string that tells ndustria how to assign dependencies. 
+                 The options are:
+                 most_recent -- Finds a Task that matches the given function name that was most recently added to the Pipeline
+                 all -- Finds all Tasks that match the given function name
+        depends_on -- A function, or list of functions whose Tasks must run before this Task can be run 
+        """
         
         self.user_function = user_function
         self.args = args
@@ -56,6 +100,7 @@ class Task:
         
 
     def __str__(self):
+        """Returns the Task name, arguments, and dependencies as a string."""
         debug_string = f"{self.user_function.__name__}("
         for i, a in enumerate(self.args):
             debug_string += str(a)
@@ -84,9 +129,11 @@ class Task:
         return debug_string
 
     def __repr__(self):
+        """Just calls str() cuz I'm lazy."""
         return str(self)
 
     def run(self):
+        """Runs the Task by calling its user_function with the supplied arguments and any dependency data"""
 
         log(f"Running {self}")
 
@@ -100,11 +147,7 @@ class Task:
 
             if self.pipeline.memcheck:
                 
-                # python 3.8 doesn't have a tracemalloc.reset_peak function, so we're faking it with a tare
-                #tracemalloc.reset_peak()
-                self.initial_mem, peak_mem_tare = tracemalloc.get_traced_memory()
-                self.peak_mem = 0
-
+                self.initial_mem, self.peak_mem = tracemalloc.get_traced_memory()
 
             ###################################################################
             # Run the actual function
@@ -115,8 +158,7 @@ class Task:
                 self.wallTime = time.time() - start
 
             if self.pipeline.memcheck:
-                self.final_mem, peak_mem = tracemalloc.get_traced_memory()
-                self.peak_mem = peak_mem
+                self.final_mem, self.peak_mem = tracemalloc.get_traced_memory()
 
         else:
             data = self.getDependencyData()
@@ -125,8 +167,7 @@ class Task:
                 start = time.time()
 
             if self.pipeline.memcheck:
-                self.initial_mem, peak_mem_tare = tracemalloc.get_traced_memory()
-                self.peak_mem = peak_mem_tare
+                self.initial_mem, self.peak_mem = tracemalloc.get_traced_memory()
 
 
             ###################################################################
@@ -138,8 +179,7 @@ class Task:
                 self.wallTime = time.time() - start
 
             if self.pipeline.memcheck:
-                self.final_mem, peak_mem = tracemalloc.get_traced_memory()
-                self.peak_mem = peak_mem
+                self.final_mem, self.peak_mem = tracemalloc.get_traced_memory()
             
         ###################################################################
         # Save the result
@@ -148,6 +188,17 @@ class Task:
         self.done = True
 
     def getDependencyData(self):
+        """Searches through its dependencies and gathers their results. 
+        
+        This method has different behavior depending on how many dependencies the Task has.
+        If no dependencies, returns None.
+        If one dependency, returns just the result of that Task
+        If multiple dependencies, the behavior depends on the match strategy used.
+            If the 'most_recent' strategy was used, it will return a dictionary keyed by function names 
+            where the values are the results of those functions
+            If the 'all' strategy was used, then it will return a list of Task results with the same 
+            ordering as the Pipeline's Task list.
+        """
 
         if self.indepedent:
             return None
@@ -168,6 +219,11 @@ class Task:
         return all_results
 
     def getFilename(self):
+        """Returns the filename in the cache that this Task saves to. May not necessarily be the same as the Task hashcode.
+        
+        I'm leaving open the possibility that the user may want to save a Task result to a specific file rather than the 
+        hash coded file in the cache. That's what this function is for.
+        """
 
         if self.filename != "":
             return self.filename
@@ -175,9 +231,7 @@ class Task:
             return self.getHashCode()
 
     def getResult(self):
-        """
-        Gets the result of this task if one exists
-        Will return None if no result exists
+        """ Gets the result of this task if one exists. Will return None if no result exists.
         """
 
         if not self.done:
@@ -192,9 +246,9 @@ class Task:
         return self.result
 
     def readyToRun(self):
-        """
-        Determines whether or not this Task is ready to be run by running
-        through its dependencies and return true if they are all marked "done"
+        """Determines whether or not this Task is ready to be run by running through its dependencies and return true if they are all marked "done"
+
+        Tasks are marked "done" when they are finished running, or when they were sent to run on another process. 
         """
 
         if self.indepedent:
@@ -206,6 +260,11 @@ class Task:
         return True
 
     def getHashCode(self):
+        """Converts the source code, arguments, and any dependency hash codes to a hash with the md5 algorithm.
+        
+        This hash gets used as a filename to save Task results in the ndustria Cache. It can be considered a unique
+        identifier of a specific instance of a Task running with a particular set of parameters.
+        """
 
         if self.hashcode != "":
             return self.hashcode
@@ -240,7 +299,7 @@ class Task:
                 target += str(k)+str(v)
             return target
 
-        # TODO: Is this actually a good idea? Whitespace changes code behavior in python
+        # Q: Is this actually a good idea? Whitespace changes code behavior in python
         # scrap the whitespace to prevent unnecessary 
         # re-queries
         source_no_ws = remove_all_whitespace(source)
--- src/ndustria.py
+++ src/ndustria.py
@@ -1,3 +1,7 @@
+"""Defines the decorators that convert user defined functions into Tasks and Views.
+
+"""
+
 import functools
 from Pipeline import Pipeline
 
@@ -5,6 +9,67 @@ def AddTask(
     depends_on=None,
     match="most_recent"
 ):
+    """When the decorated function is called, it creates a Task and adds it the Pipeline instead of running. 
+
+    Any positional or keyword arguments passed to the function are saved and passed along to the user function
+    at execution time. 
+
+    Arguments:
+    depends_on -- A function, or list of functions whose Tasks must be run before this Task can be executed
+    match -- a string that tells ndustria how to assign dependencies. 
+             The options are:
+             most_recent -- Finds a Task that matches the given function name that was most recently added to the Pipeline
+             all -- Finds all Tasks that match the given function name
+
+    -----------------------------------------------------------------------------------------
+    IMPORTANT Part I
+    -----------------------------------------------------------------------------------------
+
+    If this Task depends on a previously run Task, then the first positional argument in the function definition
+    MUST represent the return value of that Task (if there are multiple dependencies, see the documentation of Task.getDependencyData
+    for more information)
+
+    When a decorated, dependent function is called, do NOT pass in that first positional argument. Ndustria will take care of that.
+    Just pass along any extraneous arguments required to run your function.
+
+    Example:
+
+    # When defining the function
+    @AddTask(depends_on=independent_function)
+    def dependent_function(result_from_independent_function, a, b, c):
+        
+        d = do_something_with( result_from_independent_function )
+
+        result = a + b * c - d
+
+        return result
+
+    # When setting up the pipeline
+
+    # run independent_function first
+    other_function()
+
+    # then call dependent_function() WITHOUT first argument
+    dependent_function(a, b, c)
+
+    # The actual execution of both functions occurs here
+    Pipeline.run()
+        
+    -----------------------------------------------------------------------------------------
+
+    -----------------------------------------------------------------------------------------
+    IMPORTANT Part II
+    -----------------------------------------------------------------------------------------
+
+    Any arguments passed to a Task's user_function must have a unique string representation
+    given by the str() function. This is due to the default behavior of the str() function
+    for classes in Python, which returns the memory address of the object. That address is almost
+    always unique to a specific run of the code and therefore produces indeterministic hashes
+    even though the underlying data might be exactly the same.
+    -----------------------------------------------------------------------------------------
+
+    
+    """
 
     pipe = Pipeline()
 
@@ -29,7 +94,34 @@ def AddView(
     match="most_recent",
     root_proc_only=False
 ):
+    """The decorated function is converted in a View and added to the Pipeline.
 
+    Views are intended to produce a human-interpretable view of data created by a Task.
+
+    Views function similarly to Tasks by differ in a few important ways. 
+
+    Firstly, Views are always executed and their results are not saved to the cache. Avoid putting long-running code
+    into Views; that's what Tasks are for. 
+
+    Secondly, Views are always dependent on a Task. There is no such thing as an independent View.
+
+    The decorated function must have its first positional argument accept the result of its dependent Tasks, same as 
+    any dependent Task. When calling the decorated function as part of regular Pipeline setup, leave out the first
+    positional argument, again, the same as any dependent Task.
+
+    Views can take positional or keyword arguments in the same way as Tasks, and they use the same matching strategies 
+    to determine what their dependencies are. 
+
+    Views can also be run soley on the root process (rank=0) if running in parallel.
+
+    Arguments:
+    looks_at -- a function, or list of functions whose Task results are represented by this View
+    match -- a string that tells ndustria how to assign dependencies. 
+             The options are:
+             most_recent -- Finds a Task that matches the given function name that was most recently added to the Pipeline
+             all -- Finds all Tasks that match the given function name
+    root_proc_only -- Runs this View only on the root process (rank = 0)
+    """
     pipe = Pipeline()
 
     def outer_wrapper(user_function):
