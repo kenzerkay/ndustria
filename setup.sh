6284c0cfe4f15bba96a3f843133c954a117f24ab
Mackenzie Ticoras <mscott3327@gmail.com>
2025-07-15 16:06:08 -0400

    Change setup to shell script and edit pyproject

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+LINE1="# Add ndustria to Path via .bashrc"
+LINE2='export PYTHONPATH="$PYTHONPATH:$HOME"'
+LINE3='export PATH="$PATH:$HOME/ndustria/bin"'
+
+if ! grep -Fxq "$LINE1" ~/.bashrc; then
+    echo "$LINE1" >> ~/.bashrc
+fi
+
+if ! grep -Fxq "$LINE2" ~/.bashrc; then
+    echo "$LINE2" >> ~/.bashrc
+    echo "✅ Added PYTHONPATH to ~/.bashrc"
+fi
+
+if ! grep -Fxq "$LINE3" ~/.bashrc; then
+    echo "$LINE3" >> ~/.bashrc
+    echo "✅ Added ndustria/bin to PATH in ~/.bashrc"
+fi
+
+echo "💡 Done! Please run: source ~/.bashrc"
